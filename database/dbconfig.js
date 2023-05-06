dacb39a9b50b76d15eb736e2fd168153253a2d3d
Ekep Obasi <arreyettaekep@gmail.com>
2023-05-06 01:53:14 +0100

    Commit message for: - setting up starter code - installing project dependencies - I started setting up database configurations in the database/dbconfig.js file - I set up environmental variables

---
+++ database/dbconfig.js
@@ -0,0 +1,8 @@
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize("", "", "", {
+  host: "",
+  dialect: "",
+});
+
+module.exports = sequelize;
---
+++ utils.js/constants.js
@@ -0,0 +1,5 @@
+const DB_NAME = process.env.DB_NAME;
+const DB_HOST = process.env.DB_HOST;
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_PASS = process.env.DB_PASS;
+const DB_LANG = process.env.DB_LANG;
